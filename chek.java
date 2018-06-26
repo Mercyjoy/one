113fd1fc9c2d63f98e3c99258aae7af683787376
Mercyjoy <mercy15joy@gmail.com>
2018-06-26 08:36:27 +0000

    chek.java

---
+++ chek.java
@@ -0,0 +1,23 @@
+# one
+import java.io.*;
+import java.util.*;
+class chek
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanne(Sytem.in);
+int n=sc.nextInt();
+if(n>0)
+{
+System.out.println("Positive");
+}
+else if(n<0)
+{
+System.out.println("Negative");
+}
+else
+{
+System.out.println("Zero");
+}
+}
+}
